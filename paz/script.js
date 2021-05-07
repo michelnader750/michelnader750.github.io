2da2f77484bb9d2f5a3d8af34d4d473dbaaf0633
Michel Nader <michelnader750@hotmail.com>
2021-05-07 19:32:00 -0300

    paz

---
+++ paz/script.js
@@ -0,0 +1,27 @@
+function parte1() {
+    window.open("/pdfs/criacao-de-valor-em-tempos-de-crise-parte-1-de-7.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function parte2() {
+    window.open("/pdfs/a-determinacao-de-nunca-deixar-para-tras-aqueles-que-enfrentam-desafios-parte-2-de-7.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function parte3() {
+    window.open("/pdfs/estabelecer-uma-acao-global-solidaria-parte-3-de-7.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function parte4() {
+    window.open("/pdfs/construir-uma-cultura-de-direitos-humanos-parte-4-de-7.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function parte5() {
+    window.open("/pdfs/diretrizes-internacionais-para-combater-doencas-infecciosas-parte-5-de-7.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function parte6() {
+    window.open("/pdfs/tpan-ponto-de-virada-na-historia-da-humanidade-parte-6-de-7.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function parte7() {
+    window.open("/pdfs/reconstituir-a-vida-em-um-mundo-pos-covid-parte-final.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function bibliografia() {
+    window.open("/pdfs/bibliografia.pdf","_blank","width=990, height=500, top=70, left=130")
+}
+function propostasDePaz() {
+    window.open("http://www.culturadepaz.org.br/propostas/","_blank","width=990, height=500, top=70, left=130")
+}
