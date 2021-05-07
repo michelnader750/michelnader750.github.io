43779cece8500cf8f24b283c2b3cc3ad52e4851e
Michel Nader <michelnader750@hotmail.com>
2021-05-07 19:47:15 -0300

    paz ajustes

--- paz/script.js
+++ paz/script.js
@@ -5,22 +5,22 @@ function parte2() {
     window.open("https://drive.google.com/file/d/1Xe8iPgtg2vIw0dS03_LckvX3slJXobIp/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function parte3() {
-    window.open("https://drive.google.com/file/d/1rxgS7d_u6jBIxn1GQ0REHF2-ndkFcjsk/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
+    window.open("https://drive.google.com/file/d/1fGeGtCe13UlL3SdN58OnYqhkHKkhKTbX/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function parte4() {
-    window.open("https://drive.google.com/file/d/1rxgS7d_u6jBIxn1GQ0REHF2-ndkFcjsk/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
+    window.open("https://drive.google.com/file/d/1ZKhGAYyKWcQUjJGvS7IRMu3Tv-j-8hNX/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function parte5() {
-    window.open("https://drive.google.com/file/d/1rxgS7d_u6jBIxn1GQ0REHF2-ndkFcjsk/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
+    window.open("https://drive.google.com/file/d/1nyG6TAQl7ywJnmtzr9wEMAy1naWpedwh/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function parte6() {
-    window.open("https://drive.google.com/file/d/1rxgS7d_u6jBIxn1GQ0REHF2-ndkFcjsk/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
+    window.open("https://drive.google.com/file/d/16D6G1Qnm1QJqz0GGV86nmr_ij1jq5l5x/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function parte7() {
-    window.open("https://drive.google.com/file/d/1rxgS7d_u6jBIxn1GQ0REHF2-ndkFcjsk/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
+    window.open("https://drive.google.com/file/d/1Xe8iPgtg2vIw0dS03_LckvX3slJXobIp/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function bibliografia() {
-    window.open("https://drive.google.com/file/d/1rxgS7d_u6jBIxn1GQ0REHF2-ndkFcjsk/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
+    window.open("https://drive.google.com/file/d/1FFi9ICAwuOjoMnTaDVXl4YPi3Ts-bLyZ/view?usp=sharing","_blank","width=990, height=500, top=70, left=130")
 }
 function propostasDePaz() {
     window.open("http://www.culturadepaz.org.br/propostas/","_blank","width=990, height=500, top=70, left=130")
