2b07d8344bef4fa234f337d7e30186f678e0011b
Michel Nader <michelnader750@hotmail.com>
2022-06-28 21:32:28 -0300

    LiveEditing() and DesignSystem() function names have been fixed

--- genexus/script.js
+++ genexus/script.js
@@ -1,12 +1,12 @@
 function DynamicTransactions() {
     window.open("https://www.youtube.com/watch?v=mg1y9_IB46Y","_blank","width=990, height=500, top=70, left=130")
 }
-function liveEditing() {
+function LiveEditing() {
     window.open("https://www.youtube.com/watch?v=EDxh86nTeLs","_blank","width=990, height=500, top=70, left=130")
 }
-function designSystem() {
+function DesignSystem() {
     window.open("https://www.youtube.com/watch?v=EEWNUaiYCtg","_blank","width=990, height=500, top=70, left=130")
 }
-function GenexusTraining() {
+function GeneXusTraining() {
     window.open("https://training.genexus.com/pt/","_blank","width=990, height=500, top=70, left=130")
 }
