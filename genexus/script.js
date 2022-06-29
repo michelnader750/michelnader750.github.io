04f4efe381bed3d7f7ef8745153187754c21a36c
Michel Nader <michelnader750@hotmail.com>
2022-06-28 21:26:42 -0300

    The genexus endpoint was created.

---
+++ genexus/script.js
@@ -0,0 +1,12 @@
+function DynamicTransactions() {
+    window.open("https://www.youtube.com/watch?v=mg1y9_IB46Y","_blank","width=990, height=500, top=70, left=130")
+}
+function liveEditing() {
+    window.open("https://www.youtube.com/watch?v=EDxh86nTeLs","_blank","width=990, height=500, top=70, left=130")
+}
+function designSystem() {
+    window.open("https://www.youtube.com/watch?v=EEWNUaiYCtg","_blank","width=990, height=500, top=70, left=130")
+}
+function GenexusTraining() {
+    window.open("https://training.genexus.com/pt/","_blank","width=990, height=500, top=70, left=130")
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+function GeneXus() {
+    window.open("https://michelnader750.github.io/genexus/","_blank","width=990, height=500, top=70, left=130")
+}
